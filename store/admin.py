5bbc969880503cdf37b77d0f8cbf4c84549b4b1b
kribst <ilassidjoba@gmail.com>
2024-06-23 12:51:16 +0100

    modification de l interface admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Vetement
 
-# Register your models here.
+
+class VetementAdmin(admin.ModelAdmin):
+    list_display = (
+        'article', 'quantite', 'prix', 'nouveau_prix', 'type',
+        'description_display', 'information_Technique_display', 'publié'
+    )
+
+    def description_display(self, obj):
+        return (obj.description[:15] + '...') if len(obj.description) > 15 else obj.description
+
+    def information_Technique_display(self, obj):
+        return (obj.information_Technique[:15] + '...') if len(
+            obj.information_Technique) > 15 else obj.information_Technique
+
+    list_editable = ('quantite', 'prix', 'nouveau_prix', 'type')
+
+
+admin.site.register(Vetement, VetementAdmin)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+
+class Vetement(models.Model):
+    TYPES = (
+        ('homme', 'Homme'),
+        ('femme', 'Femme'),
+        ('enfant', 'Enfant'),
+        ('bebe', 'Bébé'),
+    )
+    article = models.CharField(max_length=50)
+    quantite = models.IntegerField()
+    prix = models.DecimalField(max_digits=10, decimal_places=2)
+    nouveau_prix = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=20, choices=TYPES)
+    description = models.TextField()
+    information_Technique = models.TextField()
+    image = models.ImageField()
+    publié = models.DateTimeField(default=datetime.now)
+
+
